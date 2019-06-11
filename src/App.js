f105bca318c1e94d19d93e98eb8012fc363edf38
JoshLong23 <josh.long93@gmail.com>
2019-06-11 22:39:05 +0100

    Message box update

--- src/App.js
+++ src/App.js
@@ -5,8 +5,18 @@ import Todo from './Todo';
 function App() {
   return (
     <div className='App'>
-      <details style={{ margin: '2rem', padding: '1rem' }}>
-        <summary style={{ color: 'blue' }}>Example Todo App</summary>
+      <details
+        style={{
+          margin: '2rem',
+          padding: '1rem',
+          cursor: 'auto',
+          textAlign: 'left',
+          background: 'rgba(100,100,100,0.1)',
+          borderRadius: '0.5rem'
+        }}
+      >
+        <br />
+        <summary style={{ color: 'blue', cursor: 'pointer' }}>Example Todo App</summary>
         This is an example todo app I built using React and Styled Components. Any items you add to
         this list are stored in React state and will not persist if you leave the page or reload
       </details>
